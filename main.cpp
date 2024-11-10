89078b14281440f4f8eec011b4d74f306c504a0c
VinniSantos2003 <vinixreduzino@gmail.com>
2024-11-10 12:43:39 -0300

    Calculo de polinomio adicionado - Rever calculo de polinomio

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include  <iostream>
+#include <math.h>
 using namespace std;
 
 int newton(int ordemAtual,int qntPontos,float** vetorOrdem,float** vetorPontos){//Função recursiva 
@@ -26,19 +27,22 @@ int newton(int ordemAtual,int qntPontos,float** vetorOrdem,float** vetorPontos){
 
 int main(){//Utilizar a main para criar 
 
-    int qntPontos(0),ordemSolicitada(0);
+    int qntPontos(0),grau(0);
     float **pontos = nullptr;
     float **ordem = nullptr;//Ponteiro onde as ordens serão guardada
+    float x(0),base(0),preRes(0),fxGrau(0),somatorioX(1),res(0);
 
     cout << "Quantidade de pontos a ser inserida: ";
     cin >> qntPontos;
-    pontos = new float*[qntPontos];//O * de ponteiro tem que ser depois do tipo por algum motivo
+    cout << "Insira o valor do X: ";
+    cin >> x;
+    cout << "Digite grau solicitado: ";
+    cin >> grau;
 
-    //cout << "Digite a ordem solicitada: ";
-    //cin >> ordemSolicitada;
+    pontos = new float*[qntPontos];//O * de ponteiro tem que ser depois do tipo por algum motivo
     ordem = (float**)malloc(qntPontos * sizeof(float*));
     ordem[0] = (float*)malloc(qntPontos*sizeof(float));
-
+    system("cls");
     for(int i = 0; i<qntPontos;i++){//Armazenar pontos
         pontos[i] = new float[2];
         cout << "Insira o Xi do ponto " << i << ": ";
@@ -53,7 +57,9 @@ int main(){//Utilizar a main para criar
         cout << "Xi: " << pontos[i][0] << " | ";
         cout << "F(xi): " << pontos[i][1] <<endl;
     }
-
+    for(int i = 0;i <qntPontos-1;i++){//Calculo de base
+        base = pontos[i+1][0] - pontos[i][0];
+    }
     for(int i = 0;i<qntPontos;i++){//Copia os pontos de X0 para a primeira fileira(ordem 0) para o vetor ordem
         ordem[0][i] = pontos[i][1];//Copiando certo
     }
@@ -61,5 +67,26 @@ int main(){//Utilizar a main para criar
     //Abrir função recursiva
     newton(1,qntPontos,ordem,pontos);
 
+    //Calculo do polinomio
+    //O tamanho do calculo vai depender do grau solicitado
+
+    /*Pn(X) = f(X0) + [Primeira ordem]F(X0)/h * (X-X0) + ...*/
+    //Somar f(X0) no final de tudo
+    for(int i = 0;i<grau;i++){
+        fxGrau = ordem[i+1][0]/(i+1*pow(base,i+1));
+        for(int j = 0;j<=i;j++){
+            somatorioX *= (x-pontos[j][0]);
+        }
+        //preRes guarda o somatório disso tudo
+        preRes = fxGrau * somatorioX;
+        fxGrau = 0;
+        somatorioX = 1;
+    }
+
+    res = ordem[0][0] + preRes;
+    
+    cout << "Resultado: " << res <<endl;
+    //Corrigir resultado: Deu uma unidade acima do esperado
+    //Res = 2.25 ao inves de 1.25
     return 0;
 }
